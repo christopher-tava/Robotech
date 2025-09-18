b15a1505d067b2491c143589d43e328ef44c38c4
Chris Tava <chris.tava@chopra.com>
2025-09-17 22:53:03 -0400

    new

--- components/main/projects.tsx
+++ components/main/projects.tsx
@@ -3,10 +3,22 @@ import { PROJECTS } from "@/constants";
 
 export const Projects = () => {
   return (
+    
     <section
       id="projects"
       className="flex flex-col items-center justify-center py-20"
     >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        The Robotech Hackathon is the annual robotics hackathon run by the IEEE
+          student chapter at Georgia Tech. Within the allotted 36 hours, teams of
+          four individuals race to design, build, and test a robot to complete a
+          set of engineering goals and competition tasks. Excellent collaboration
+          and an element of strategy will be required to maximize a team’s score.
+          Teams compete in one of several tracks differentiated by competition focus
+          and experience. Products are judged by a panel of judges on the last day
+          of the hackathon.
+      </h1>
+
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Schedule of Events
       </h1>
