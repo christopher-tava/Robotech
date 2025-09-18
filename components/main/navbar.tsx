49ff77e218177c8f1a5e8da8b83c42e466e6eaf3
Chris Tava <chris.tava@chopra.com>
2025-09-17 23:54:53 -0400

    Save local changes

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { LINKS, NAV_LINKS, SOCIALS } from "@/constants";
+import { LINKS, NAV_LINKS } from "@/constants";
 
 export const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -45,19 +45,7 @@ export const Navbar = () => {
           </div>
         </div>
 
-        {/* Social Icons (Web) */}
-        <div className="hidden md:flex flex-row gap-5">
-          {SOCIALS.map(({ link, name, icon: Icon }) => (
-            <Link
-              href={link}
-              target="_blank"
-              rel="noreferrer noopener"
-              key={name}
-            >
-              <Icon className="h-6 w-6 text-white" />
-            </Link>
-          ))}
-        </div>
+        
 
         {/* Hamburger Menu */}
         <button
