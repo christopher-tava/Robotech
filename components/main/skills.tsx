60997ed439d2685faa0a913c0022788a4c00a042
Chris Tava <chris.tava@chopra.com>
2025-09-18 00:01:27 -0400

    Save local changes

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -1,7 +1,12 @@
 import { SkillDataProvider } from "@/components/sub/skill-data-provider";
 import { SkillText } from "@/components/sub/skill-text";
 
-
+import {
+  BACKEND_SKILL,
+  FRONTEND_SKILL,
+  FULLSTACK_SKILL,
+  SKILL_DATA,
+} from "@/constants";
 
 export const Skills = () => {
   return (
@@ -12,6 +17,55 @@ export const Skills = () => {
     >
       <SkillText />
 
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {SKILL_DATA.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {FRONTEND_SKILL.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {BACKEND_SKILL.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {FULLSTACK_SKILL.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {OTHER_SKILL.map((skill, i) => (
           <SkillDataProvider
